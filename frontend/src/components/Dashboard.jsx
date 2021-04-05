3cef863e580b783d7c8d41e8b88056a6222418a7
mehahalabe <meha.halabe@utexas.edu>
2021-04-05 15:38:11 -0500

    Included new project button

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,22 +1,23 @@
 // src/components/Home.jsx
 import React from "react";
-import { Table } from 'reactstrap';
 import './style.css';
-import auth from "../auth";
-import {Button} from "reactstrap";
+
 
 var divStyle = {
     color:'white'
 };
 
 class Dashboard extends React.Component {
+    nextPath(path) {
+        this.props.history.push(path);
+      }
+
     render () {
         return (
-
             <div className="container">
                  <div className="card-wrapper">
                     <div class="card1">
-                        <h3>Welcome to your dashboard!</h3>
+                        <h3><strong>Welcome</strong> to your dashboard{/*{this.props.location.state.name}*/}</h3>
                         <table striped bordered hover></table>
                         <thead>
                             <tr>
@@ -25,7 +26,7 @@ class Dashboard extends React.Component {
                             <th>Date Created</th>
                             </tr>
                         </thead>
-                        <button class = "bigbtn" variant = "outline-primary">NEW PROJECT</button>
+                        <button onClick={() => this.nextPath('/newproject')} class = "bigbtn" variant = "outline-primary">NEW PROJECT</button>
                     </div>
                 </div>    
             </div>
