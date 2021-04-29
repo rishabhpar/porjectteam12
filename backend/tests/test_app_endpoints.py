f24f5294a0fdc50df779d1935f1e5a19c0026d64
rishabhpar <rishabh.parekh@utexas.edu>
2021-04-28 22:16:48 -0500

    transferred rish tests to amit's master file

--- backend/tests/test_app_endpoints.py
+++ backend/tests/test_app_endpoints.py
@@ -2,31 +2,130 @@ from unittest import mock
 from nose.tools import assert_true
 import requests
 import json
+import random
+import string
 
 # To test use the following command: nosetests --verbosity=2 --nocapture test_app_endpoints.py 
 # The --nocapture flag allows for the print statements to show up
 
-#This is to test that the login endpoint is up and returns an ok response
-def test_login_endpoint():
+#These are 4 tests that the login endpoint is up and returns an ok response
+def test_login_endpoint_bad_email():
     # Send a request to the login endpoint and store the response
-    login_credentials = {"email":"am@gmail.com","password":"Hello123!"}
+    login_credentials = {"email": "happyhappy@lilducky.com", "password": "Testing123!"}
     endpoint = "http://backendteam12.herokuapp.com/api/login"
     response = requests.post(endpoint, json=login_credentials)
 
-    print(response.json())
+    assert response.json()["error"] == "Entered email is not associated with a user"
 
-    assert_true(response.ok)
 
-# This is to test that the register endpoint is up and returns an ok response
-def test_register_endpoint():
+def test_login_endpoint_bad_password():
+    # Send a request to the login endpoint and store the response
+    login_credentials = {"email": "testingme@gmail.com", "password": "Hi"}
+    endpoint = "http://backendteam12.herokuapp.com/api/login"
+    response = requests.post(endpoint, json=login_credentials)
+
+    assert response.json()["error"] == "Incorrect Password"
+
+
+def test_login_endpoint_empty_form():
+    # Send a request to the login endpoint and store the response
+    login_credentials = {"email": "", "password": ""}
+    endpoint = "http://backendteam12.herokuapp.com/api/login"
+    response = requests.post(endpoint, json=login_credentials)
+
+    assert response.json()["error"] == "Entered email is not associated with a user"
+
+
+def test_login_endpoint_happy_case():
+    # Send a request to the login endpoint and store the response
+    f = open("test_password.txt", "r")
+    password = f.readline()
+
+    login_credentials = {"email": "jigglypuff@test.com", "password": password}
+    f.close()
+    endpoint = "http://backendteam12.herokuapp.com/api/login"
+    response = requests.post(endpoint, json=login_credentials)
+
+
+    assert response.json()["success"] == True
+
+###################################################################################################################################
+
+# These 8 tests that the register endpoint is up and returns an ok response
+def test_register_endpoint_user_already_exists():
     # Send a request to the register endpoint and store the response
-    register_credentials = {"name":"name","email":"a@gmail.com","password":"Hello123!","confirm_password":"Hello123!"}
+    register_credentials = {"email": "rishabh.parekh@utexas.edu", "password": "Testing123!", "confirm_password":"Testing123!", "name":"Rish"}
     endpoint = "http://backendteam12.herokuapp.com/api/register"
     response = requests.post(endpoint, json=register_credentials)
 
-    print(response.json())
+    assert response.json()["error"] == "User with that email already exists"
 
-    assert_true(response.ok)
+def test_register_endpoint_weak_password_characters():
+    # Send a request to the register endpoint and store the response
+    register_credentials = {"email": "testingme@gmail.com", "password": "Hi", "confirm_password":"Hi", "name":"Rish"}
+    endpoint = "http://backendteam12.herokuapp.com/api/register"
+    response = requests.post(endpoint, json=register_credentials)
+
+    assert response.json()["error"] == "Password is not strong enough. Minimums: 8 characters, 1 uppercase, 1 number, 1 special"
+
+
+def test_register_endpoint_weak_password_uppercase():
+    # Send a request to the register endpoint and store the response
+    register_credentials = {"email": "testingme@gmail.com", "password": "testing123!", "confirm_password":"testing123!", "name":"Rish"}
+    endpoint = "http://backendteam12.herokuapp.com/api/register"
+    response = requests.post(endpoint, json=register_credentials)
+
+    assert response.json()["error"] == "Password is not strong enough. Minimums: 8 characters, 1 uppercase, 1 number, 1 special"
+
+
+def test_register_endpoint_weak_password_numbers():
+    # Send a request to the register endpoint and store the response
+    register_credentials = {"email": "testingme@gmail.com", "password": "Testing!", "confirm_password":"Testing!", "name":"Rish"}
+    endpoint = "http://backendteam12.herokuapp.com/api/register"
+    response = requests.post(endpoint, json=register_credentials)
+
+    assert response.json()["error"] == "Password is not strong enough. Minimums: 8 characters, 1 uppercase, 1 number, 1 special"
+
+
+def test_register_endpoint_weak_password_specials():
+    # Send a request to the register endpoint and store the response
+    register_credentials = {"email": "testingme@gmail.com", "password": "Testing123", "confirm_password":"Testing123", "name":"Rish"}
+    endpoint = "http://backendteam12.herokuapp.com/api/register"
+    response = requests.post(endpoint, json=register_credentials)
+
+    assert response.json()["error"] == "Password is not strong enough. Minimums: 8 characters, 1 uppercase, 1 number, 1 special"
+
+
+def test_register_endpoint_mismatched_passwords():
+    # Send a request to the register endpoint and store the response
+    register_credentials = {"email": "testingme@gmail.com", "password": "Testing123!", "confirm_password":"Testing1!", "name":"Rish"}
+    endpoint = "http://backendteam12.herokuapp.com/api/register"
+    response = requests.post(endpoint, json=register_credentials)
+
+    assert response.json()["error"] == "Passwords do not match"
+
+
+def test_register_endpoint_empty_form():
+    # Send a request to the register endpoint and store the response
+    register_credentials = {"email": "", "password": "", "confirm_password":"", "name":""}
+    endpoint = "http://backendteam12.herokuapp.com/api/register"
+    response = requests.post(endpoint, json=register_credentials)
+
+    assert response.json()["error"] == "Password is not strong enough. Minimums: 8 characters, 1 uppercase, 1 number, 1 special"
+
+
+def test_register_endpoint_happy_case():
+    # printing lowercase
+    letters = string.ascii_lowercase
+    title = ''.join(random.choice(letters) for i in range(10)) 
+    # Send a request to the register endpoint and store the response
+    register_credentials = {"email": title + "@testing.com", "password": "Testing123!", "confirm_password":"Testing123!", "name":"Rish"}
+    endpoint = "http://backendteam12.herokuapp.com/api/register"
+    response = requests.post(endpoint, json=register_credentials)
+
+    assert response.json()["success"] == True
+
+###################################################################################################################################
 
 # This is to test that the newproject endpoint is up and returns an ok response
 def test_newproject_endpoint():
@@ -83,16 +182,57 @@ def test_projectdetails_endpoint():
 
     assert_true(response.ok)
 
+###############################################################################################################################
+
 # This is to test that the updatepassword endpoint is up and returns an ok response
-def test_updatepassword_endpoint():
+def test_updatepassword_endpoint_nonexisting_user():
     # Send a request to the updatepassword endpoint and store the response
-    password_data = {"currentPassword": "Hello123!", "newPassword":"Bye123!!", "accountType":"user", "email":"a@gmail.com"}
+    password_data = {"email": "happyhappy@lilducky.com", "currentPassword": "Testing123!", "newPassword": "newPass123!", "accountType": "user"}
     endpoint = "http://backendteam12.herokuapp.com/api/updatepassword"
     response = requests.post(endpoint, json=password_data)
 
-    print(response.json())
+    assert response.json()["error"] == "Entered email is not associated with a user"
 
-    assert_true(response.ok)
+
+def test_updatepassword_endpoint_incorrect_password():
+    # Send a request to the updatepassword endpoint and store the response
+    password_data = {"email": "jigglypuff@test.com", "currentPassword": "hihihihi", "newPassword": "newpass", "accountType": "user"}
+    endpoint = "http://backendteam12.herokuapp.com/api/updatepassword"
+    response = requests.post(endpoint, json=password_data)
+
+    assert response.json()["error"] == "Incorrect Password"
+
+
+def test_updatepassword_endpoint_empty_form():
+    # Send a request to the updatepassword endpoint and store the response
+    password_data = {"email": "", "currentPassword": "", "newPassword": "", "accountType": "user"}
+    endpoint = "http://backendteam12.herokuapp.com/api/updatepassword"
+    response = requests.post(endpoint, json=password_data)
+
+    assert response.json()["error"] == "Entered email is not associated with a user"
+
+
+def test_updatepassword_endpoint_happy_case():
+    # Send a request to the updatepassword endpoint and store the response
+    letters = string.ascii_lowercase
+    newpass = ''.join(random.choice(letters) for i in range(10))
+
+    f = open("test_password.txt", "r")
+    password = f.readline()
+    f.close()
+
+    password_data = {"email": "jigglypuff@test.com", "currentPassword": password, "newPassword": newpass, "accountType": "user"}
+    
+    f = open("test_password.txt", "w")
+    f.write(newpass)
+    f.close()
+
+    endpoint = "http://backendteam12.herokuapp.com/api/updatepassword"
+    response = requests.post(endpoint, json=password_data)
+
+    assert response.json()["success"] == True
+
+####################################################################################################################################
 
 # This is to test that the getdatasets endpoint is up and returns an ok response
 def test_getdatasets_endpoint():
